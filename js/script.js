d0e1b998a5791a3868d68f79d611bbd4bd2ec870
Yang Zhou <yzhou3991@gmail.com>
2018-04-03 20:42:42 -0400

    chain error() method after Ajax request

--- js/script.js
+++ js/script.js
@@ -18,12 +18,15 @@ function loadData() {
     var address = $street + ', ' + $city;
     var url = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address;
     $body.append('<img class = bgimg src = "' + url + '">');
+
     $.getJSON('....' + $city + '....', function(data){
       var articles = data.response.docs;
       $.each(articles,function(article){
-        $('#nytimes-articles').append('<li class = "article"><a href =' + article.web_url
+        $nytElem.append('<li class = "article"><a href =' + article.web_url
          + '>' + article.headline.main + '</a><p>' + article.snippet + '</p></li>';)
       })
+    }).error(function(){
+      $nytElem.append('<p>Error!</p>');
     })
 
 
