a9fa10dcc4a1d55752a3aa16cbe9cfccc46596fc
Yang Zhou <yzhou3991@gmail.com>
2018-04-03 22:31:51 -0400

    add wiki API

--- js/script.js
+++ js/script.js
@@ -19,17 +19,28 @@ function loadData() {
     var url = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address;
     $body.append('<img class = bgimg src = "' + url + '">');
 
-    $.getJSON('....' + $city + '....', function(data){
-      var articles = data.response.docs;
-      $.each(articles,function(article){
-        $nytElem.append('<li class = "article"><a href =' + article.web_url
-         + '>' + article.headline.main + '</a><p>' + article.snippet + '</p></li>';)
-      })
-    }).error(function(){
-      $nytElem.append('<p>Error!</p>');
+    // $.getJSON('....' + $city + '....', function(data){
+    //   var articles = data.response.docs;
+    //   $.each(articles,function(article){
+    //     $nytElem.append('<li class = "article"><a href =' + article.web_url
+    //      + '>' + article.headline.main + '</a><p>' + article.snippet + '</p></li>';)
+    //   })
+    // }).error(function(){
+    //   $nytElem.append('<p>Error!</p>');
+    // })
+
+    $.ajax('https://en.wikipedia.org/w/api.php?action=opensearch&search=' + $city +
+    '&format=json&callback=wikiCallback',{
+      dataType: 'jsonp',
+      success: function(response){
+        var articles = response[1];
+        for (var i = 0; i < articles.length; i ++){
+          var article = articles[i];
+          var url = 'http://en.wikipedia.org/wiki/' + article;
+          $wikiElem.append('<li><a href ="' + url + '">' + article + '</a></li>');
+        }
+      }
     })
-
-
     return false;
 };
 
